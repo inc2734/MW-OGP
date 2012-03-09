6a77dc14f7eaf2b94b12f0a1c1ee93d45a6a0dfd
inc2734 <inc@2inc.org>
2012-03-09 18:07:36 +0900

    Classing the admin page.

--- admin_page.php
+++ admin_page.php
@@ -1,164 +1,179 @@
 <?php
-// Update Settings
-if ( isset( $_POST['submit'] ) ) {
-	if ( ! current_user_can( 'manage_options' ) )
-		die( __( 'You cannot edit the search-by-category options.' ) );
-	check_admin_referer( 'mw_ogp-updatesettings' );
+class mw_ogp_admin_page {
 
-	// Get our new option values
-	// Update the DB with the new option values
-	update_option( 'mw_ogp', array(
-		'app_id' => $_POST['app_id'],
-		'type' => $_POST['type'],
-		'image' => $_POST['image'],
-		'locale' => $_POST['locale'],
-	) );
-}
+	public $types = array(
+		'Activities' => array(
+			'activity',
+			'sport'
+		),
+		'Businesses' => array(
+			'bar',
+			'company',
+			'cafe',
+			'hotel',
+			'restaurant'
+		),
+		'Groups' => array(
+			'cause',
+			'sports_league',
+			'sports_team'
+		),
+		'Organizations' => array(
+			'band',
+			'government',
+			'non_profit',
+			'school',
+			'university'
+		),
+		'People' => array(
+			'actor',
+			'athlete',
+			'author',
+			'director',
+			'musician',
+			'politician',
+			'public_figure'
+		),
+		'Places' => array(
+			'city',
+			'country',
+			'landmark',
+			'state_province'
+		),
+		'Products and Entertainment' => array(
+			'album',
+			'book',
+			'drink',
+			'food',
+			'game',
+			'product',
+			'song',
+			'movie',
+			'tv_show'
+		),
+		'Websites' => array(
+			'blog',
+			'website',
+			'article'
+		)
+	);
+	
+	public $locales = array(
+		'Afrikaans' => 'af_ZA',
+		'Arabic' => 'ar_AR',
+		'Azeri' => 'az_AZ',
+		'Belarusian' => 'be_BY',
+		'Bulgarian' => 'bg_BG',
+		'Bengali' => 'bn_IN',
+		'Bosnian' => 'bs_BA',
+		'Catalan' => 'ca_ES',
+		'Czech' => 'cs_CZ',
+		'Welsh' => 'cy_GB',
+		'Danish' => 'da_DK',
+		'German' => 'de_DE',
+		'Greek' => 'el_GR',
+		'English (UK)' => 'en_GB',
+		'English (Pirate)' => 'en_PI',
+		'English (Upside Down)' => 'en_UD',
+		'English (US)' => 'en_US',
+		'Esperanto' => 'eo_EO',
+		'Spanish (Spain)' => 'es_ES',
+		'Spanish' => 'es_LA',
+		'Estonian' => 'et_EE',
+		'Basque' => 'eu_ES',
+		'Persian' => 'fa_IR',
+		'Leet Speak' => 'fb_LT',
+		'Finnish' => 'fi_FI',
+		'Faroese' => 'fo_FO',
+		'French (Canada)' => 'fr_CA',
+		'French (France)' => 'fr_FR',
+		'Frisian' => 'fy_NL',
+		'Irish' => 'ga_IE',
+		'Galician' => 'gl_ES',
+		'Hebrew' => 'he_IL',
+		'Hindi' => 'hi_IN',
+		'Croatian' => 'hr_HR',
+		'Hungarian' => 'hu_HU',
+		'Armenian' => 'hy_AM',
+		'Indonesian' => 'id_ID',
+		'Icelandic' => 'is_IS',
+		'Italian' => 'it_IT',
+		'Japanese' => 'ja_JP',
+		'Georgian' => 'ka_GE',
+		'Khmer' => 'km_KH',
+		'Korean' => 'ko_KR',
+		'Kurdish' => 'ku_TR',
+		'Latin' => 'la_VA',
+		'Lithuanian' => 'lt_LT',
+		'Latvian' => 'lv_LV',
+		'Macedonian' => 'mk_MK',
+		'Malayalam' => 'ml_IN',
+		'Malay' => 'ms_MY',
+		'Norwegian (bokmal)' => 'nb_NO',
+		'Nepali' => 'ne_NP',
+		'Dutch' => 'nl_NL',
+		'Norwegian (nynorsk)' => 'nn_NO',
+		'Punjabi' => 'pa_IN',
+		'Polish' => 'pl_PL',
+		'Pashto' => 'ps_AF',
+		'Portuguese (Brazil)' => 'pt_BR',
+		'Portuguese (Portugal)' => 'pt_PT',
+		'Romanian' => 'ro_RO',
+		'Russian' => 'ru_RU',
+		'Slovak' => 'sk_SK',
+		'Slovenian' => 'sl_SI',
+		'Albanian' => 'sq_AL',
+		'Serbian' => 'sr_RS',
+		'Swedish' => 'sv_SE',
+		'Swahili' => 'sw_KE',
+		'Tamil' => 'ta_IN',
+		'Telugu' => 'te_IN',
+		'Thai' => 'th_TH',
+		'Filipino' => 'tl_PH',
+		'Turkish' => 'tr_TR',
+		'Ukrainian' => 'uk_UA',
+		'Vietnamese' => 'vi_VN',
+		'Simplified Chinese (China)' => 'zh_CN',
+		'Traditional Chinese (Hong Kong)' => 'zh_HK',
+		'Traditional Chinese (Taiwan)' => 'zh_TW'
+	);
 
-// Get Current DB Values
-$options = get_option( 'mw_ogp' );
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+	}
 
-$types = array(
-	'Activities' => array(
-		'activity',
-		'sport'
-	),
-	'Businesses' => array(
-		'bar',
-		'company',
-		'cafe',
-		'hotel',
-		'restaurant'
-	),
-	'Groups' => array(
-		'cause',
-		'sports_league',
-		'sports_team'
-	),
-	'Organizations' => array(
-		'band',
-		'government',
-		'non_profit',
-		'school',
-		'university'
-	),
-	'People' => array(
-		'actor',
-		'athlete',
-		'author',
-		'director',
-		'musician',
-		'politician',
-		'public_figure'
-	),
-	'Places' => array(
-		'city',
-		'country',
-		'landmark',
-		'state_province'
-	),
-	'Products and Entertainment' => array(
-		'album',
-		'book',
-		'drink',
-		'food',
-		'game',
-		'product',
-		'song',
-		'movie',
-		'tv_show'
-	),
-	'Websites' => array(
-		'blog',
-		'website',
-		'article'
-	)
-);
+	/**
+	 * do update_option
+	 *  @return	Boolean
+	 */
+	private function _updateOption() {
+		$updateFlg = update_option( 'mw_ogp', array(
+			'app_id' => $_POST['app_id'],
+			'type' => $_POST['type'],
+			'image' => $_POST['image'],
+			'locale' => $_POST['locale'],
+		) );
+		return $updateFlg;
+	}
 
-$locales = array(
-	'Afrikaans' => 'af_ZA',
-	'Arabic' => 'ar_AR',
-	'Azeri' => 'az_AZ',
-	'Belarusian' => 'be_BY',
-	'Bulgarian' => 'bg_BG',
-	'Bengali' => 'bn_IN',
-	'Bosnian' => 'bs_BA',
-	'Catalan' => 'ca_ES',
-	'Czech' => 'cs_CZ',
-	'Welsh' => 'cy_GB',
-	'Danish' => 'da_DK',
-	'German' => 'de_DE',
-	'Greek' => 'el_GR',
-	'English (UK)' => 'en_GB',
-	'English (Pirate)' => 'en_PI',
-	'English (Upside Down)' => 'en_UD',
-	'English (US)' => 'en_US',
-	'Esperanto' => 'eo_EO',
-	'Spanish (Spain)' => 'es_ES',
-	'Spanish' => 'es_LA',
-	'Estonian' => 'et_EE',
-	'Basque' => 'eu_ES',
-	'Persian' => 'fa_IR',
-	'Leet Speak' => 'fb_LT',
-	'Finnish' => 'fi_FI',
-	'Faroese' => 'fo_FO',
-	'French (Canada)' => 'fr_CA',
-	'French (France)' => 'fr_FR',
-	'Frisian' => 'fy_NL',
-	'Irish' => 'ga_IE',
-	'Galician' => 'gl_ES',
-	'Hebrew' => 'he_IL',
-	'Hindi' => 'hi_IN',
-	'Croatian' => 'hr_HR',
-	'Hungarian' => 'hu_HU',
-	'Armenian' => 'hy_AM',
-	'Indonesian' => 'id_ID',
-	'Icelandic' => 'is_IS',
-	'Italian' => 'it_IT',
-	'Japanese' => 'ja_JP',
-	'Georgian' => 'ka_GE',
-	'Khmer' => 'km_KH',
-	'Korean' => 'ko_KR',
-	'Kurdish' => 'ku_TR',
-	'Latin' => 'la_VA',
-	'Lithuanian' => 'lt_LT',
-	'Latvian' => 'lv_LV',
-	'Macedonian' => 'mk_MK',
-	'Malayalam' => 'ml_IN',
-	'Malay' => 'ms_MY',
-	'Norwegian (bokmal)' => 'nb_NO',
-	'Nepali' => 'ne_NP',
-	'Dutch' => 'nl_NL',
-	'Norwegian (nynorsk)' => 'nn_NO',
-	'Punjabi' => 'pa_IN',
-	'Polish' => 'pl_PL',
-	'Pashto' => 'ps_AF',
-	'Portuguese (Brazil)' => 'pt_BR',
-	'Portuguese (Portugal)' => 'pt_PT',
-	'Romanian' => 'ro_RO',
-	'Russian' => 'ru_RU',
-	'Slovak' => 'sk_SK',
-	'Slovenian' => 'sl_SI',
-	'Albanian' => 'sq_AL',
-	'Serbian' => 'sr_RS',
-	'Swedish' => 'sv_SE',
-	'Swahili' => 'sw_KE',
-	'Tamil' => 'ta_IN',
-	'Telugu' => 'te_IN',
-	'Thai' => 'th_TH',
-	'Filipino' => 'tl_PH',
-	'Turkish' => 'tr_TR',
-	'Ukrainian' => 'uk_UA',
-	'Vietnamese' => 'vi_VN',
-	'Simplified Chinese (China)' => 'zh_CN',
-	'Traditional Chinese (Hong Kong)' => 'zh_HK',
-	'Traditional Chinese (Taiwan)' => 'zh_TW'
-);
-?>
+	/**
+	 * view
+	 */
+	public function view() {
+		if ( isset( $_POST['submit'] ) ) {
+			if ( ! current_user_can( 'manage_options' ) )
+				die( __( 'You cannot edit the search-by-category options.' ) );
+			check_admin_referer( 'mw_ogp-updatesettings', 'mw_ogp-updatesettings' );
+			$updateFlg = $this->_updateOption();
+		}
+		$options = get_option( 'mw_ogp' );
+		?>
 <div class="wrap">
 	<?php screen_icon( 'edit-pages' ); ?>
 	<h2>MW OGP</h2>
-	<?php if ( isset( $_POST['submit'] ) ) : ?>
+	<?php if ( !empty( $updateFlg ) ) : ?>
 	<div class="updated"><p><strong>Settings has been updated.</strong></p></div>
 	<?php endif; ?>
 	<ul>
@@ -168,7 +183,7 @@ $locales = array(
 	</ul>
 	<form action="" method="post" id="mw_ogp-config">
 		<table class="form-table">
-			<?php if ( function_exists( 'wp_nonce_field' ) ) wp_nonce_field( 'mw_ogp-updatesettings' ); ?>
+			<?php if ( function_exists( 'wp_nonce_field' ) ) wp_nonce_field( 'mw_ogp-updatesettings', 'mw_ogp-updatesettings' ); ?>
 			<tr>
 				<th scope="row" valign="top" style="width:20%"><label for="app_id">fb:app_id</label></th>
 				<td><input type="text" name="app_id" id="app_id" class="regular-text" value="<?php echo esc_html( $options['app_id'] ); ?>"/></td>
@@ -177,11 +192,10 @@ $locales = array(
 				<th scope="row" valign="top"><label for="type">og:type( Front Page )<br />Defaut : blog</label></th>
 				<td>
 					<select name="type" id="type">
-						<?php foreach ( $types as $optgroupLbl => $optgroup ) : ?>
+						<?php foreach ( $this->types as $optgroupLbl => $optgroup ) : ?>
 						<optgroup label="<?php echo esc_html( $optgroupLbl ); ?>">
 							<?php foreach ( $optgroup as $type ) : ?>
-							<?php $selected = ( $options['type'] == $type ) ? ' selected="selected"' : ''; ?>
-							<option value="<?php echo esc_html( $type ); ?>"<?php echo $selected; ?>><?php echo esc_html( $type ); ?></option>
+							<option value="<?php echo esc_html( $type ); ?>"<?php selected( $options['type'], $type ); ?>><?php echo esc_html( $type ); ?></option>
 							<?php endforeach; ?>
 						</optgroup>
 						<?php endforeach; ?>
@@ -196,9 +210,8 @@ $locales = array(
 				<th scope="row" valign="top"><label for="locale">og:locale<br />Defaut : Japanese</label></th>
 				<td>
 					<select name="locale" id="locale">
-						<?php foreach ( $locales as $localeLbl => $locale ) : ?>
-							<?php $selected = ( $options['locale'] == $locale ) ? ' selected="selected"' : ''; ?>
-							<option value="<?php echo esc_html( $locale ); ?>"<?php echo $selected; ?>><?php echo esc_html( $localeLbl ); ?></option>
+						<?php foreach ( $this->locales as $localeLbl => $locale ) : ?>
+						<option value="<?php echo esc_html( $locale ); ?>"<?php selected( $options['locale'], $locale ); ?>><?php echo esc_html( $localeLbl ); ?></option>
 						<?php endforeach; ?>
 					</select>
 				</td>
@@ -208,3 +221,7 @@ $locales = array(
 		<span class="submit" style="border: 0;"><input type="submit" name="submit" value="Save Settings" /></span>
 	</form>
 </div>
+		<?php
+	}
+}
+?>
--- mw_ogp.php
+++ mw_ogp.php
@@ -3,7 +3,7 @@
  * Plugin Name: MW OGP
  * Plugin URI: http://2inc.org
  * Description: Added FB Scripts, div#fb-root, OGP tags.
- * Version: 0.1.2
+ * Version: 0.2
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * License: GPL2
@@ -23,182 +23,188 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+include( 'admin_page.php' );
+
 register_activation_hook( __FILE__, array( 'mw_ogp', 'activation' ) );
 register_uninstall_hook( __FILE__, array( 'mw_ogp', 'uninstall' ) );
 
-if ( ! class_exists( 'mw_ogp' ) ) {
+$mw_ogp = new mw_ogp();
+$mw_ogp->add_action();
+$mw_ogp->add_filter();
 
-	class mw_ogp {
-		public $app_id = '';
-		public $options = array();
+class mw_ogp {
 
-		/**
-		 * Constructer
-		 */
-		public function __construct() {
-			$options = get_option( 'mw_ogp' );
+	public static $options = array(
+		'app_id' => '',
+		'type' => 'blog',
+		'image' => '',
+		'locale' => 'ja_JP'
+	);
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$options = get_option( 'mw_ogp' );
+		if ( !empty( $options ) ) {
 			$this->options = $options;
-			$app_id = ( empty( $options['app_id'] ) ) ? '' : strval( $options['app_id'] );
-			$this->app_id = $app_id;
-			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
-		}
-
-		/**
-		 * activation
-		 */
-		public static function activation() {
-			add_option( 'mw_ogp', array(
-				'app_id' => '',
-				'type' => 'blog',
-				'image' => '',
-				'locale' => 'ja_JP'
-			) );
-		}
-
-		/**
-		 * uninstall
-		 */
-		public static function uninstall() {
-			delete_option( 'mw_ogp' );
-		}
-
-		/**
-		 * add_admin_menu
-		 */
-		public function add_admin_menu() {
-			add_options_page( 'MW OGP', 'MW OGP', 8, __FILE__,  array( $this, 'admin_page' ) );
-		}
-
-		/**
-		 * admin_page
-		 */
-		public function admin_page() {
-			include( 'admin_page.php' );
-		}
-
-		/**
-		 * do action!
-		 */
-		public function add_filter() {
-			add_filter( 'wp_head', array( $this, 'print_head' ) );
-			add_filter( 'wp_footer', array( $this, 'print_footer' ) );
-		}
-
-		/**
-		 * Added tags in head
-		 */
-		public function print_head() {
-			$image = $this->options['image'];
-			if ( is_singular() && !is_front_page() ) {
-				$type = 'article';
-				$url = get_permalink();
-				$title = get_the_title();
-				if ( $_image = $this->catch_that_image() ) {
-					$image = $_image;
-				}
-			} else {
-				$type = ( empty( $this->options['type'] ) ) ? 'blog' : $this->options['type'];
-				$url = home_url();
-				$title = get_bloginfo( 'name' );
-			}
-
-			$options = array(
-				'type' => $type,
-				'url' => $url,
-				'title' => $title,
-				'image' => $image,
-				'site_name' => get_bloginfo( 'name' ),
-				'description' => $this->get_description(),
-				'locale' => $this->options['locale']
-			);
-	
-			echo sprintf( '
-				<meta property="fb:app_id" content="%s" />
-				<meta property="og:type" content="%s" />
-				<meta property="og:site_name" content="%s" />
-				<meta property="og:image" content="%s" />
-				<meta property="og:title" content="%s" />
-				<meta property="og:url" content="%s" />
-				<meta property="og:description" content="%s" />
-				<meta property="og:locale" content="%s" />
-			', esc_attr( $this->app_id ), esc_attr( $options['type'] ), esc_attr( $options['site_name'] ), esc_attr( $options['image'] ) ,esc_attr( $options['title'] ), esc_attr( $options['url'] ), esc_attr( $options['description'] ), esc_attr( $options['locale'] ) );
-		}
-
-		/**
-		 * Added tags in footer
-		 */
-		public function print_footer() {
-			echo sprintf( '
-				<div id="fb-root"></div>
-				<script type="text/javascript">
-				window.fbAsyncInit = function() {
-					FB.init({
-						appId  : "%s",
-						status : true,
-						cookie : true,
-						xfbml  : true,
-						channelURL : "%s",
-						oauth  : true
-					});
-				};
-				(function() {
-					var e = document.createElement("script");
-					e.src = document.location.protocol + "//connect.facebook.net/%s/all.js";
-					e.async = true;
-					document.getElementById("fb-root").appendChild(e);
-				}());
-				</script>
-			', esc_html( $this->app_id ), get_permalink(), esc_html( $this->options['locale'] ) );
-		}
-
-		/**
-		 * catch_that_image 
-		 */
-		public function catch_that_image() {
-			global $post;
-			$first_img = '';
-		
-			$image_id = get_post_thumbnail_id();
-			if ( $image_id ) {
-				$image_url = wp_get_attachment_image_src( $image_id, 'thumbnail', true );
-			}
-			
-			if ( !empty( $image_url[0] ) ) {
-				$first_img = $image_url[0];
-			} else {
-				$output = preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/msi', $post->post_content, $matches );
-				$first_img = $matches[1][0];
-			}
-			if ( ! empty( $first_img ) && preg_match( '/^\/.+$/', $first_img ) ) {
-				$first_img = home_url().$first_img;
-			}
-			return $first_img;
-		}
-
-		/**
-		 * get_description 
-		 */
-		public function get_description( $strnum = 200 ) {
-			global $post;
-			if ( is_singular() && !is_front_page() ) {
-				if ( !empty( $post->post_excerpt ) ) {
-					$description = $post->post_excerpt;
-				} elseif ( !empty( $post->post_content ) ) {
-					$description = $post->post_content;
-				}
-			} else {
-				$description = get_bloginfo( 'description' );
-			}
-			$description = strip_tags( $description );
-			$description = esc_html( $description );
-			$description = str_replace( array( "\r\n","\r","\n" ), '', $description );
-			$description = mb_strimwidth( $description, 0, $strnum, "…", 'utf8' );
-			return $description;
 		}
 	}
 
-	// $mw_ogp = new mw_ogp( '', array( 'type' => 'website' ) );
-	$mw_ogp = new mw_ogp();
-	$mw_ogp->add_filter();
+	/**
+	 * activation
+	 */
+	public static function activation() {
+		add_option( 'mw_ogp', self::$options );
+	}
+
+	/**
+	 * uninstall
+	 */
+	public static function uninstall() {
+		delete_option( 'mw_ogp' );
+	}
+
+	/**
+	 * do action!
+	 */
+	public function add_action() {
+		add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+	}
+
+	/**
+	 * do filter!
+	 */
+	public function add_filter() {
+		add_filter( 'wp_head', array( $this, 'print_head' ) );
+		add_filter( 'wp_footer', array( $this, 'print_footer' ) );
+	}
+
+	/**
+	 * add_admin_menu
+	 */
+	public function add_admin_menu() {
+		add_options_page( 'MW OGP', 'MW OGP', 8, __FILE__,  array( $this, 'admin_page' ) );
+	}
+
+	/**
+	 * admin_page
+	 */
+	public function admin_page() {
+		$admin_page = new mw_ogp_admin_page();
+		$admin_page->view();
+	}
+
+	/**
+	 * Added tags in head
+	 */
+	public function print_head() {
+		$image = $this->options['image'];
+		if ( is_singular() && !is_front_page() ) {
+			$type = 'article';
+			$url = get_permalink();
+			$title = get_the_title();
+			if ( $_image = $this->catch_that_image() ) {
+				$image = $_image;
+			}
+		} else {
+			$type = ( empty( $this->options['type'] ) ) ? 'blog' : $this->options['type'];
+			$url = home_url();
+			$title = get_bloginfo( 'name' );
+		}
+
+		$options = array(
+			'type' => $type,
+			'url' => $url,
+			'title' => $title,
+			'image' => $image,
+			'site_name' => get_bloginfo( 'name' ),
+			'description' => $this->get_description(),
+			'locale' => $this->options['locale']
+		);
+
+		echo sprintf( '
+			<meta property="fb:app_id" content="%s" />
+			<meta property="og:type" content="%s" />
+			<meta property="og:site_name" content="%s" />
+			<meta property="og:image" content="%s" />
+			<meta property="og:title" content="%s" />
+			<meta property="og:url" content="%s" />
+			<meta property="og:description" content="%s" />
+			<meta property="og:locale" content="%s" />
+		', esc_attr( $options['app_id'] ), esc_attr( $options['type'] ), esc_attr( $options['site_name'] ), esc_attr( $options['image'] ) ,esc_attr( $options['title'] ), esc_attr( $options['url'] ), esc_attr( $options['description'] ), esc_attr( $options['locale'] ) );
+	}
+
+	/**
+	 * Added tags in footer
+	 */
+	public function print_footer() {
+		echo sprintf( '
+			<div id="fb-root"></div>
+			<script type="text/javascript">
+			window.fbAsyncInit = function() {
+				FB.init({
+					appId  : "%s",
+					status : true,
+					cookie : true,
+					xfbml  : true,
+					oauth  : true
+				});
+			};
+			(function() {
+				var e = document.createElement("script");
+				e.src = document.location.protocol + "//connect.facebook.net/%s/all.js";
+				e.async = true;
+				document.getElementById("fb-root").appendChild(e);
+			}());
+			</script>
+		', esc_html( $options['app_id'] ), esc_html( $this->options['locale'] ) );
+	}
+
+	/**
+	 * catch_that_image 
+	 */
+	public function catch_that_image() {
+		global $post;
+		$first_img = '';
+	
+		$image_id = get_post_thumbnail_id();
+		if ( $image_id ) {
+			$image_url = wp_get_attachment_image_src( $image_id, 'thumbnail', true );
+		}
+		
+		if ( !empty( $image_url[0] ) ) {
+			$first_img = $image_url[0];
+		} else {
+			$output = preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/msi', $post->post_content, $matches );
+			$first_img = $matches[1][0];
+		}
+		if ( ! empty( $first_img ) && preg_match( '/^\/.+$/', $first_img ) ) {
+			$first_img = home_url().$first_img;
+		}
+		return $first_img;
+	}
+
+	/**
+	 * get_description 
+	 */
+	public function get_description( $strnum = 200 ) {
+		global $post;
+		if ( is_singular() && !is_front_page() ) {
+			if ( !empty( $post->post_excerpt ) ) {
+				$description = $post->post_excerpt;
+			} elseif ( !empty( $post->post_content ) ) {
+				$description = $post->post_content;
+			}
+		} else {
+			$description = get_bloginfo( 'description' );
+		}
+		$description = strip_tags( $description );
+		$description = esc_html( $description );
+		$description = str_replace( array( "\r\n","\r","\n" ), '', $description );
+		$description = mb_strimwidth( $description, 0, $strnum, "…", 'utf8' );
+		return $description;
+	}
 }
 ?>
